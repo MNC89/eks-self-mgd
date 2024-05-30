ba87beb433b6c0ffc8b64f5ad605dd9fa303d3dc
MNC89 <kurz17@gmail.com>
2024-05-30 11:52:56 -0500

    add igw and route tables

--- vpc/resources.tf
+++ vpc/resources.tf
@@ -7,6 +7,46 @@ resource "aws_vpc" "fp_vpc" {
   }
 }
 
+### Internet Gateway ###
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.fp_vpc.id
+  tags = {
+    Name = "final-project-internet-gateway"
+  }
+}
+
+### Public Route Table ###
+
+resource "aws_route_table" "pub_rt" {
+  vpc_id = aws_vpc.fp_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "final-project-public-route-table"
+  }
+}
+
+### Public Route Table Association ###
+
+resource "aws_route_table_association" "pub_rt_association_1" {
+  subnet_id      = aws_subnet.pub_sub_1.id
+  route_table_id = aws_route_table.pub_rt.id
+}
+
+resource "aws_route_table_association" "pub_rt_association_2" {
+  subnet_id      = aws_subnet.pub_sub_2.id
+  route_table_id = aws_route_table.pub_rt.id
+}
+
+resource "aws_route_table_association" "pub_rt_association_3" {
+  subnet_id      = aws_subnet.pub_sub_3.id
+  route_table_id = aws_route_table.pub_rt.id
+}
+
 ### VPC Public Subnets ### ***USE FOR EACH ON FINAL VERSION ***
 
 resource "aws_subnet" "pub_sub_1" {
@@ -61,7 +101,7 @@ resource "aws_subnet" "priv_sub_2" {
   }
 }
 
-resource "aws_subnet" "pub_sub_3" {
+resource "aws_subnet" "priv_sub_3" {
   vpc_id            = aws_vpc.fp_vpc.id
   cidr_block        = "10.0.160.0/20"
   availability_zone = "us-east-1c"
