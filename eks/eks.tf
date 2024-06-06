2da4d74f1f2867cf57b14650b2c6403ce6c8ff60
MNC89 <kurz17@gmail.com>
2024-06-06 11:40:05 -0500

    move node iam blocks from eks.tf to workers.tf and add instance profile

--- eks/eks.tf
+++ eks/eks.tf
@@ -49,47 +49,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   role       = aws_iam_role.eks_iam_role.name
 }
 
-### EKS Node IAM Role ###
-
-data "aws_iam_policy_document" "node_assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "node_iam_role" {
-  name               = "fp-eks-worker-node-role"
-  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node_iam_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node_iam_role.name
-}
-
-#SSM role required?
-resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.node_iam_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node_iam_role.name
-}
-
 ### EKS OIDC ### https://docs.aws.amazon.com/eks/latest/userguide/cni-iam-role.html
 
 data "tls_certificate" "eks_tls" {
--- eks/workers.tf
+++ eks/workers.tf
@@ -68,8 +68,8 @@ resource "aws_launch_template" "fp_asg_lt" {
     }
   }
 
-  iam_instance_profile { #to be configured
-    name = "test"
+  iam_instance_profile { 
+    name = aws_iam_instance_profile.node_instance_profile.name
   }
 
   tag_specifications {
@@ -135,3 +135,48 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+### EKS Node IAM Role ###
+
+data "aws_iam_policy_document" "node_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_instance_profile" "node_instance_profile" {
+  name = "fp-eks-worker-node-instance-profile"
+  role = aws_iam_role.node_iam_role.name
+}
+
+resource "aws_iam_role" "node_iam_role" {
+  name               = "fp-eks-worker-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_iam_role.name
+}
+
+#SSM role required?
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.node_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_iam_role.name
+}
