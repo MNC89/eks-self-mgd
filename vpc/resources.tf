e03f6fbf9dc3947cf04c2cd2bd713337aa782140
MNC89 <kurz17@gmail.com>
2024-06-06 20:01:19 -0500

    enable dns hostnames in vpc

--- vpc/resources.tf
+++ vpc/resources.tf
@@ -1,7 +1,8 @@
 ### VPC ###
 
 resource "aws_vpc" "fp_vpc" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
   tags = {
     Name = var.vpc_name
   }
