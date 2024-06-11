a8d16f15980f5411453062a7e4d2d7a96b9d2167
MNC89 <kurz17@gmail.com>
2024-06-10 19:15:56 -0500

    eks policy document

--- eks/eks.tf
+++ eks/eks.tf
@@ -77,8 +77,9 @@ data "aws_iam_policy_document" "eks_oidc_assume_role_policy" {
 
 ### EBS CSI ###
 
+
 resource "aws_iam_openid_connect_provider" "eks" {
-  url             = aws_eks_cluster.main.identity.0.oidc.0.issuer
+  url             = aws_eks_cluster.fp_eks_cluster.identity[0].oidc[0].issuer
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
 }
